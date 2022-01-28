91537ccdd8e4554b9725df0ef8d49bb46c2e3a64
NazV <70192835+NazV@users.noreply.github.com>
2022-01-28 15:36:19 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,3 +7,19 @@ const swiper = new Swiper('.swiper', {
     slideToClickedSlide: true,
     toggle: true,
 });
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.main-display__button');
+
+buttonModal.addEventListener('click', () => {
+    modalWindow.classList.add('active');
+});
+
+modalWindow.addEventListener('click', (e) => {
+    const isModal = e.target.closest('.modal__inner');
+    if (!isModal) {
+        modalWindow.classList.remove('active');
+    } 
+})
+
+
